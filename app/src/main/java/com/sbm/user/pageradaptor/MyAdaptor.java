72d1119c8e468965fcba1612157cd539e9ded379
JCln <ghmantera@gmail.com>
2017-09-15 04:00:15 +0430

    fragment

--- app/src/main/java/com/sbm/user/pageradaptor/MyAdaptor.java
+++ app/src/main/java/com/sbm/user/pageradaptor/MyAdaptor.java
@@ -1,44 +1,26 @@
 package com.sbm.user.pageradaptor;
-
-import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
 
 import java.util.ArrayList;
-
-
 // ...
-
-    public  class Myadaptor extends FragmentStatePagerAdapter {
+    public class MyAdaptor extends FragmentStatePagerAdapter {
         ArrayList<PageWrapper> countries= new ArrayList();
         int mCount;
 
-        public Myadaptor(FragmentManager fm, ArrayList<PageWrapper> countries) {
+        public MyAdaptor(FragmentManager fm, ArrayList<PageWrapper> countries) {
             super(fm);
             this.countries = countries;
-
-
-        }
-
+         }
         @Override
         public int getCount() {
             //notifyDataSetChanged();
           mCount=countries.size();
             return mCount;
         }
-
         public Fragment getItem(int ps) {
             return Firstfragment.newInstance(countries.get(ps));
         }
-       /* @Override
-        public int getItemPosition(Object object) {
-            return PagerAdapter.POSITION_NONE;
-        }*/
-
-
-
     }
 
